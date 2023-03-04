5fd5b4f87bfb15e73e9018df1c4d4d9caf70f47f
tony <cheekygit@forensic.coffee>
2023-03-04 13:38:58 -0600

    added comments.pl, script to print comments from file/url

---
+++ comments.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# / comments.pl: quick & dirty tool to show comments from a file or http/s URL /
+# \ @anthonykava                                               2023-03-04.1337 \
+# //////////////////////////////////////////////////////////////////////////////
+
+use strict;                                                 # of course
+use warnings;                                               # why not?
+
+# $url as argument, if not ^http then we assume it's a file
+my $url=shift()||die("\nusage: $0 'url|filename'\n\n");     # needs a single arg
+$url='file:'.$url if &ltrim($url)!~/^http/i;                # files get file:
+$url=~s/[\'\"\`\!]//g;                                      # minor sanitisation
+
+# we'll be running $cmd and parsing its output lines
+my $cmd="wget --no-check-certificate -qO - '${url}'";       # default wget
+if($url=~/^file:(.+)$/) {                                   # if file: ...
+    die("file not found: ${url}") if !-e $1;                # check exists
+    $cmd="cat '$1'";                                        # cat for files
+}
+
+# tofu and potatoes
+my $in=0;                                                   # flag for in block
+my $ln=0;                                                   # line number
+foreach(`${cmd}`) {                                         # iter. on output
+    $ln++;                                                  # increment line no.
+    chomp();                                                # no new lines
+    s/^\s+//;                                               # trim leading space
+    s/\s+$//;                                               # trim trailing
+    if(/((\/\/|\#.+)$)/) {                                  # // or # ...
+        $_=$1;
+        printf("%05d: %s\n",$ln,$_)                         # ignore colours
+            if !/[\:\"\'\s](\#[A-Fa-f0-9]{3}|\#[A-Fa-f0-9]{6})[\;\"\'\s]/;
+    } elsif(/(\/\*.*)$/) {                                  # start of a block
+        $_=$1;
+        s/(\*\/).*$/$1/;                                    # trim trailing
+        printf("%05d: %s\n",$ln,$_);
+        $in=1 unless /\*\//;                                # set flag unless
+    } elsif($in) {                                          # lines in block
+        printf("%05d: \\-- %s\n",$ln,$1) if  /(.*\*\/).*$/;
+        printf("%05d: \\-- %s\n",$ln,$_) if !/(.*\*\/).*$/;
+        $in=0 if /\*\//;                                    # unset flag
+    }
+}
+
+# ltrim($_='') -- trim leading spaces from string $_ and return it
+sub ltrim {
+    $_=shift()||'';
+    s/^\s+//;
+    return($_);
+}
